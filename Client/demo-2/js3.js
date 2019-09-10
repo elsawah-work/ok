79b2647580595d8ee7df58aeb22e6a14ce9155ea
elsawah-work <elsawah.demo@gmail.com>
2019-09-11 01:40:49 +0300

    sa
    sad

---
+++ Client/demo-2/js3.js
@@ -0,0 +1,61 @@
+<!--
+ document.write("<script src='https://rawgit.com/elsawah-work/ok/master/C"
+ +"lient/Corporate-02/jquery.min.js' type='text/javascript'/>
+    <script"
+ +" src='https://rawgit.com/elsawah-work/ok/master/js/loading.js'/>
+    <"
+ +"script src='https://rawgit.com/elsawah-work/ok/master/js/bootstrap.min"
+ +".js'/>
+    <script src='https://rawgit.com/elsawah-work/ok/master/js/b"
+ +"ootsnav.js'/>
+    <script src='https://rawgit.com/elsawah-work/ok/mast"
+ +"er/js/revolution/jquery.themepunch.tools.min.js' type='text/javascript"
+ +"'/>
+    <script src='https://rawgit.com/elsawah-work/ok/master/js/revo"
+ +"lution/jquery.themepunch.revolution.min.js' type='text/javascript'/>
+ "
+ +"   <script src='https://rawgit.com/elsawah-work/ok/master/js/revolutio"
+ +"n/revolution.extension.layeranimation.min.js' type='text/javascript'/>"
+ +"
+    <script src='https://rawgit.com/elsawah-work/ok/master/js/revolut"
+ +"ion/revolution.extension.navigation.min.js' type='text/javascript'/>
+ "
+ +"   <script src='https://rawgit.com/elsawah-work/ok/master/js/revolutio"
+ +"n/revolution.extension.parallax.min.js' type='text/javascript'/>
+    <"
+ +"script src='https://rawgit.com/elsawah-work/ok/master/js/revolution/re"
+ +"volution.extension.slideanims.min.js' type='text/javascript'/>
+    <sc"
+ +"ript src='https://rawgit.com/elsawah-work/ok/master/js/revolution/sett"
+ +"ing/clean-revolution-slider.js' type='text/javascript'/>
+    <script s"
+ +"rc='https://rawgit.com/elsawah-work/ok/master/js/form/jcf.js'/>
+    <s"
+ +"cript src='https://rawgit.com/elsawah-work/ok/master/js/form/jcf.selec"
+ +"t.js'/>
+    <script src='https://rawgit.com/elsawah-work/ok/master/js/"
+ +"masonry/masonry.min.js'/>
+    <script src='https://rawgit.com/elsawah-"
+ +"work/ok/master/js/masonry/masonry.filter.js'/>
+    <script src='https:"
+ +"//rawgit.com/elsawah-work/ok/master/js/masonry/setting.js'/>
+    <scri"
+ +"pt src='https://rawgit.com/elsawah-work/ok/master/js/prettyPhoto/jquer"
+ +"y.prettyPhoto.js'/>
+    <script src='https://rawgit.com/elsawah-work/o"
+ +"k/master/js/prettyPhoto/setting.js'/>
+    <script src='https://rawgit."
+ +"com/elsawah-work/ok/master/js/owlcarousel/owl.carousel.min.js'/>
+    <"
+ +"script src='https://rawgit.com/elsawah-work/ok/master/js/owlcarousel/s"
+ +"etting.js'/>
+    <script src='https://rawgit.com/elsawah-work/ok/maste"
+ +"r/js/jplayer/jquery.jplayer.js'/>
+    <script src='https://rawgit.com/"
+ +"elsawah-work/ok/master/js/jplayer/setting.js'/>
+    <script src='https"
+ +"://rawgit.com/elsawah-work/ok/master/js/custom.js'/>
+    <script src='"
+ +"https://rawgit.com/elsawah-work/ok/master/js/template-option/demosetti"
+ +"ng.js'/>");
+-->
